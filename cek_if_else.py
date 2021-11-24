8003cb622e6046c46c014ad9274d6bdf10366e5c
Azmi Alfatih S <73034464+alfatihs@users.noreply.github.com>
2021-11-24 16:05:15 +0700

    cek_if_else

---
+++ cek_if_else.py
@@ -0,0 +1,17 @@
+nama = "azmi"
+umur_5_tahun_lalu = 23
+print ("nama saya")
+print(nama)
+print("sedangkan umur saat ini adalah")
+print(umur_5_tahun_lalu + 5)
+
+
+
+if (nama == "nathan") :
+    print("selamat datang Nathan!")
+elif (nama == "azmi") :
+    print("selamat datang Azmi!")
+else : 
+    print("Selamat datang pak")
+    print(nama)
+
